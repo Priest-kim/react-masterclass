31ae264a9dfcf0112e997bf04c3071bed9b204ae
jaden <hyunbin.kim.3376@gmail.com>
2023-04-29 15:58:07 +0900

    typescript optional

--- react-masterclass/typescript/src/Circle.tsx
+++ react-masterclass/typescript/src/Circle.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 interface ContainerProps {
     bgColor: string;
+    borderColor?: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -9,19 +10,21 @@ const Container = styled.div<ContainerProps>`
     height: 100px;
     background-color: ${({ bgColor }) => bgColor};
     border-radius: 50px;
+    border: 1px solid ${({ borderColor }) => borderColor};
 `;
 
 interface CircleProps {
     bgColor: string;
+    borderColor?: string;
+    text?: string;
 }
 
-const Circle = ({ bgColor }: CircleProps) => {
-    return <Container bgColor={bgColor} />;
+const Circle = ({ bgColor, borderColor, text = 'default text' }: CircleProps) => {
+    return (
+        <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+            {text}
+        </Container>
+    );
 };
 
 export default Circle;
-
-interface PlayerShape {
-    name: string;
-    age: number;
-}
