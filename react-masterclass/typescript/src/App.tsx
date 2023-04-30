499bd3c2e524c7cac23a9707ea67f91e390a1096
jaden <hyunbin.kim.3376@gmail.com>
2023-04-30 09:05:54 +0900

    typescript connext styled-components

--- react-masterclass/typescript/src/App.tsx
+++ react-masterclass/typescript/src/App.tsx
@@ -1,25 +1,19 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    background-color: ${({ theme }) => theme.bgColor};
+`;
+
+const Title = styled.h1`
+    color: ${({ theme }) => theme.textColor};
+`;
 
 function App() {
-    const [value, setValue] = useState('');
-    const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: { value },
-        } = e;
-        setValue(value);
-    };
-
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log('hello', value);
-    };
     return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input value={value} onChange={onChange} type="text" placeholder="username" />
-                <button>Log in</button>
-            </form>
-        </div>
+        <Container>
+            <Title>Hello</Title>
+        </Container>
     );
 }
 
